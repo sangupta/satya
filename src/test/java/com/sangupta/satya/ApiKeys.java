1f6539ed1f9881cd99adaec21b28571108168e35
Sandeep Gupta <sandy.pec@gmail.com>
2014-04-04 19:31:09 +0530

    Adding empty keys so that compilation can succeed during CI builds

---
+++ src/test/java/com/sangupta/satya/ApiKeys.java
@@ -0,0 +1,31 @@
+package com.sangupta.satya;
+
+/**
+ * Various API keys that we use for testing samples and others.
+ * 
+ * @author sangupta
+ *
+ */
+public interface ApiKeys {
+
+	public static final String Facebook = null;
+
+	public static final String FacebookSecret = null;
+
+	public static final String GoogleSecret = null;
+
+	public static final String Google = null;
+
+	public static final String MicrosoftLive = null;
+
+	public static final String MicrosoftLiveSecret = null;
+
+	public static final String TwitterSecret = null;
+
+	public static final String Twitter = null;
+
+	public static final String Yahoo = null;
+
+	public static final String YahooSecret = null;
+	
+}
