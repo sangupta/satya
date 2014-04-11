75a2715744ff547689df8946db2c9c747865267b
Sandeep Gupta <sandy.pec@gmail.com>
2014-04-11 15:01:27 +0530

    Added null keys for all providers of version 1.0

--- src/test/java/com/sangupta/satya/ApiKeys.java
+++ src/test/java/com/sangupta/satya/ApiKeys.java
@@ -1,31 +1,65 @@
+/**
+ *
+ * satya - social authentication framework
+ * Copyright (c) 2014, Sandeep Gupta
+ * 
+ * http://sangupta.com/projects/satya
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.sangupta.satya;
 
 /**
- * Various API keys that we use for testing samples and others.
+ * Basic definition of API keys that are used by all examples
+ * to demo out the functionality.
  * 
  * @author sangupta
- *
+ * @since 1.0
  */
 public interface ApiKeys {
+	
+	public static final String Google = null;
+	
+	public static final String GoogleSecret = null;
 
 	public static final String Facebook = null;
 
 	public static final String FacebookSecret = null;
 
-	public static final String GoogleSecret = null;
-
-	public static final String Google = null;
-
 	public static final String MicrosoftLive = null;
 
 	public static final String MicrosoftLiveSecret = null;
 
-	public static final String TwitterSecret = null;
-
 	public static final String Twitter = null;
+
+	public static final String TwitterSecret = null;
 
 	public static final String Yahoo = null;
 
 	public static final String YahooSecret = null;
+
+	public static final String LinkedIn = null;
 	
+	public static final String LinkedInSecret = null;
+
+	public static final String Github = null;
+
+	public static final String GithubSecret = null;
+
+	public static final String DropBox = null;
+
+	public static final String DropBoxSecret = null;
+
 }
